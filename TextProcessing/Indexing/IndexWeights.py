2fc04c1eaaa2430bf6c16a8057937bb3b1a65c99
Chris <Chris_de_Rijcke@hotmail.com>
2019-02-27 19:01:02 +0100

    add weight to dict script

---
+++ TextProcessing/Indexing/IndexWeights.py
@@ -0,0 +1,71 @@
+# global import
+import pickle
+import sys
+import math
+
+# local import
+import IndexerFunctions as IF
+import IndexingMachineFunctions as IMF
+import IndexingMultiple as IM
+#######
+
+# get weights per documents by tf-idf way
+
+# iterate over document:
+    # get total amount of doc
+    # iterate over tf per doc:
+        # per tf, calculate tf-idf
+        # append value to weight key
+
+
+
+def addWeights2Dict(IndexDict):
+    dictCounter = 0
+    lenghtDict = len(IndexDict)
+    TotalDocs = getTotalDocsInCorpus()
+    for term in IndexDict:
+        documentFreq = IndexDict[term]["documentFrequency"]
+        termFreqencies = IndexDict[term]["termFrequencies"]
+        for termFreq in termFreqencies:
+            IndexDict[term]["weights"].append(getWeightValue(TotalDocs, termFreq, documentFreq))
+        dictCounter += 1
+        if dictCounter % math.ceil(lenghtDict/5) == 0:
+            IMF.addTime("weighting at {}%".format((math.ceil((dictCounter/lenghtDict)*100))))
+    IMF.addTime("weighting complete")
+    return IndexDict
+
+# helper functions
+def getWeightValue(TotDocs, termFreq, docFreq):
+    idf = getIDF(TotDocs, docFreq)
+    rawTF_IDF = termFreq * idf
+    roundTF_IDF = round(rawTF_IDF,5)
+    return roundTF_IDF
+
+def getIDF(TotDocs, docFreq):
+    return math.log10(TotDocs / docFreq)
+
+def getTotalDocsInCorpus():
+    try:
+        path2csv = IM.getMainMovieCSV()
+        numberDocs = IMF.getCSVLength(path2csv)
+    except:
+        numberDocs = 70000
+    return numberDocs
+
+def saveNewDict(WeightDict):
+    path2save = "C:\\Users\\chris\\OneDrive\\Documenten\\IR_DS2019\\TextProcessing\\Indexing\\MultipleIndexLogFiles\\MainIndexFile\\"
+    fullPath = path2save + "FullWeightedIndex"
+    IF.saveDict(WeightDict, fullPath, ".pkl")
+    IMF.addTime("Dictionary with weights has been saved in folder: \n" + path2save)
+
+# load dict
+def getDict():
+    path2dict = "C:\\Users\\chris\\OneDrive\\Documenten\\IR_DS2019\\TextProcessing\\Indexing\\SavedIndexes\\FullIndexes\\FullFirstIndex_2019-02-22.pkl"
+    pickle_in = open(path2dict,"rb")
+    return pickle.load(pickle_in)
+
+
+IndexDict = getDict()
+DictWithWeights = addWeights2Dict(IndexDict)
+saveNewDict(DictWithWeights)
+
