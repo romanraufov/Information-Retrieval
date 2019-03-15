c38c760bc53d2f385291a104e9bce4681cb44533
Chris <Chris_de_Rijcke@hotmail.com>
2019-03-15 16:45:50 +0100

    python files setting for "static"

--- search_engine/search_engine/settings.py
+++ search_engine/search_engine/settings.py
@@ -119,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
